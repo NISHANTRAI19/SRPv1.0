3e4e50b49192260dc73825aed01605a28eb61e9b
Nishant Rai <rai7nishant@gmail.com>
2023-09-10 13:03:15 +0530

    somewhat cool but basic

---
+++ srpv/src/App.js
@@ -0,0 +1,18 @@
+import Analytics from "./components/Analytics";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <Analytics />
+
+    </div>
+  );
+}
+
+export default App;
